15b569475bf8ae7ee9a5f736cce6e9b09e1365a4
Alec Hoffmann <astrohoff@ufl.edu>
2017-03-18 12:46:23 -0400

    Created a HealthManager script for managing object health. Created a Damage Zone prefab for damaging an object with a HealthManager. After being damaged, the HealthManager will make objects invicible for a specified time. When damaging an object, the Damage Zone will push the object back in a direction that depends on what part of the damage zone was touched.

---
+++ Assets/Scripts/HealthManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthManager : MonoBehaviour {
+    public float health = 2;
+    public float invincibilityTime = 1f;
+    public bool isInvicible = false;
+
+    public void TakeDamage(float damage)
+    {
+        if (!isInvicible)
+        {
+            health -= damage;
+            if (health <= 0)
+            {
+                Die();
+            }
+            else
+            {
+                StartCoroutine(InvicibilityRoutine());
+            }
+        }
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    private IEnumerator InvicibilityRoutine()
+    {
+        isInvicible = true;
+        yield return new WaitForSeconds(invincibilityTime);
+        isInvicible = false;
+    }
+}
