4dfb621dbb667aa8fbf3272587a6a582a3cc9a17
Alec Hoffmann <astrohoff@ufl.edu>
2017-03-24 12:31:27 -0400

    Updated default variable values for LogicManager and KeyDetector scripts on prefabs. Changed KeyDetector to use a trigger to detect when a Player is close to the door so that the triggering area can be visualized.

--- Assets/Scripts/KeyDetector.cs
+++ Assets/Scripts/KeyDetector.cs
@@ -2,50 +2,56 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// Class for detecting if the player has a key.
 public class KeyDetector : MonoBehaviour {
     private GameObject player;
+    // Player's KeyManager component.
     private KeyManager keyManager;
+    // Key detector's logic manager.
     private LogicManager logicManager;
-    public float senseDistance = 2f;
+    // If true, KeyManager will be checked for a key with the below requieredKeyID value.
+    // If false, the KeyManager will be checked for any key.
     public bool checkKeyId = false;
     public int requiredKeyId = 0; 
 
     private void Awake()
     {
-        player = GameObject.Find("Player");
+        player = GameObject.FindGameObjectWithTag("Player");
         keyManager = player.GetComponent<KeyManager>();
         logicManager = GetComponent<LogicManager>();
     }
-	
-	// Update is called once per frame
-	void Update () {
-        // Check for a key if detector hasn't found anything yet.
+
+    private void OnTriggerStay2D(Collider2D c)
+    {
+        // If a key has not already been detected...
         if (!logicManager.GetState())
         {
-            // Check if player is close enough.
-            if ((player.transform.position - transform.position).magnitude < senseDistance)
+            // If the player has entered the detection zone...
+            if(c.tag == "Player")
             {
-                // If checking for a specific key...
-                if (checkKeyId)
-                {
-                    // Do activation stuff if the key is found.
-                    if (keyManager.CheckHasKey(requiredKeyId))
-                    {
-                        keyManager.RemoveKey(requiredKeyId);
-                        logicManager.SetState(true);
-                    }
-                }
-                // If any key will do...
-                else
-                {
-                    // Do activation stuff if a key is found.
-                    if (keyManager.CheckHasKey())
-                    {
-                        keyManager.RemoveKey();
-                        logicManager.SetState(true);
-                    }
-                }
+                CheckForKey();               
             }
         }
-	}
+    }
+
+    private void CheckForKey()
+    {
+        // Use appropriate KeyManager methods for whether the key ID should be checked (checkKeyId).
+        if (checkKeyId)
+        {
+            if (keyManager.CheckHasKey(requiredKeyId))
+            {
+                keyManager.RemoveKey(requiredKeyId);
+                logicManager.SetState(true);
+            }
+        }
+        else
+        {
+            if (keyManager.CheckHasKey())
+            {
+                keyManager.RemoveKey();
+                logicManager.SetState(true);
+            }
+        }
+    }
 }
