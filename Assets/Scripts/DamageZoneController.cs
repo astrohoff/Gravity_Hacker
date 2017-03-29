a310cc1e2597ba8c5b21fbb7d10f481c50ee27b0
Alec Hoffmann <astrohoff@ufl.edu>
2017-03-29 14:30:27 -0400

    Updated Damage Zone prefab to use spikes sprite. Updated DamageZone script to use collider center and handle corner collisions better. Updated button and door LogiManager variables in tutorial scene.

--- Assets/Scripts/DamageZoneController.cs
+++ Assets/Scripts/DamageZoneController.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class DamageZoneController : MonoBehaviour {
     public float damageAmount = 1;
     public float pushbackAmount = 1f;
+    // The size of the area
     public float pushbackZonePadding = 0.1f;
 
     private BoxCollider2D damageZoneCollider;
@@ -29,53 +30,75 @@ public class DamageZoneController : MonoBehaviour {
         }
     }
 
-    // Get the direction an object should be pushed in when it contacts the damage zone.
-    // Returns direction in world space.
+    // Gets the direction to push the player based on what side of the collider was hit.
     private Vector2 GetPushBackDirection(Collision2D collision)
     {
-        // Get average collision position.
-        Vector2 averagePoint = Vector2.zero;
-        for(int i = 0; i < collision.contacts.Length; i++)
-        {
-            averagePoint += collision.contacts[i].point;
-        }
-        averagePoint /= collision.contacts.Length;
-        // Convert position from world space to local (damage zone) space.
-        averagePoint = transform.InverseTransformPoint(averagePoint);
+        // Get the average collision point (in case there are multiple).
+        Vector2 collisionPoint = GetAverageCollisionPoint(collision);
 
-        // Check for the different possible collision positions and return corresponding directions.
-        Vector2 colliderMax = transform.InverseTransformPoint(damageZoneCollider.bounds.max);
-        Vector2 colliderMin = transform.InverseTransformPoint(damageZoneCollider.bounds.min);
-        Vector2 padding = damageZoneCollider.bounds.extents * pushbackZonePadding;
-        Vector2 pushBackDirection;
-        // Between zone sides.
-        if (averagePoint.x < colliderMax.x  - padding.x && averagePoint.x > colliderMin.x + padding.x) {
-            // Top.
-            if (averagePoint.y >= 0)
-            {
-                pushBackDirection = Vector2.up;
-            }
-            // Bottom.
-            else
-            {
-                pushBackDirection = Vector2.down;
-            }
-        }
-        // On zone sides.
-        else
+        // Determine direction based on what part of the collider was hit.
+        // Because collisions can happen below the surface of a collider (due to finite physics resolution)
+        // determining the side hit from the collision position can be ambiguous around corners.
+        // To handle this, vertical and horizontal sides are checked independantly and can both
+        // be added to the pushback direction.
+        // Padding is added to side bounds a zone where a side hit is recognized.
+        Vector2 pushbackDirection = Vector2.zero;
+        // Get vertical pushback.
+        // Check if between left and right sides.
+        if(collisionPoint.x > damageZoneCollider.bounds.min.x + pushbackZonePadding)
         {
-            // Right.
-            if(averagePoint.x > 0)
+            if(collisionPoint.x <= damageZoneCollider.bounds.max.x - pushbackZonePadding)
             {
-                pushBackDirection = Vector2.right;
-            }
-            // Left.
-            else
-            {
-                pushBackDirection = Vector2.left;
+                // If top...
+                if(collisionPoint.y > damageZoneCollider.bounds.center.y)
+                {
+                    pushbackDirection += Vector2.up;
+                }
+                // If bottom...
+                else
+                {
+                    pushbackDirection += Vector2.down;
+                }
             }
         }
-        // To do: corners.
-        return pushBackDirection;
+        // Add horizontal pushback.
+        // Check if between top and bottom.
+        if (collisionPoint.y >= damageZoneCollider.bounds.min.y + pushbackZonePadding)
+        {
+            if (collisionPoint.y < damageZoneCollider.bounds.max.y - pushbackZonePadding)
+            {
+                // If left...
+                if(collisionPoint.x < damageZoneCollider.bounds.center.x)
+                {
+                    pushbackDirection += Vector2.left;
+                }
+                // If right.
+                else
+                {
+                    pushbackDirection += Vector2.right;
+                }
+            }
+        }
+        // If a corner was hit, pushbackDirection will still  be zero.
+        // In this case just use the direction from the center of the collider to the hit point.
+        if(pushbackDirection == Vector2.zero)
+        {
+            pushbackDirection = collisionPoint - (Vector2)damageZoneCollider.bounds.center;
+        }
+        // Normalize so that the magnitude is conistent.
+        pushbackDirection.Normalize();
+        return pushbackDirection;
+    }
+
+
+    private Vector2 GetAverageCollisionPoint(Collision2D collision)
+    {
+        Vector2 collisionPoint = Vector2.zero;
+        for (int i = 0; i < collision.contacts.Length; i++)
+        {
+            collisionPoint += collision.contacts[i].point;
+        }
+        collisionPoint = collisionPoint / collision.contacts.Length;
+        return collisionPoint;
     }
 }
