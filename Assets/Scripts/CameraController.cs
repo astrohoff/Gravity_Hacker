9bbb648596abd0b3a12af502c9e39e777b66eed6
BESANT\Besan_000 <besantnandra@gmail.com>
2017-04-14 15:54:47 -0400

    level 1 done, working on level 2

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -22,10 +22,11 @@ public class CameraController : MonoBehaviour {
 				height = 3.8f;//target.position.y + 2.8f;
 				Vector3 point = gameObject.GetComponent<Camera> ().WorldToViewportPoint (target.position);
 				Vector3 temp = new Vector3 (.5f, .5f, point.z);
-				float delta = target.position.x - gameObject.GetComponent<Camera> ().ViewportToWorldPoint (temp).x;
-				Vector3 destination = new Vector3 (transform.position.x + delta, height, transform.position.z);
+				//float delta = target.position.x - gameObject.GetComponent<Camera> ().ViewportToWorldPoint (temp).x;
+				float deltaY = target.position.y - gameObject.GetComponent<Camera> ().ViewportToWorldPoint (temp).y;
+				Vector3 destination = new Vector3 (transform.position.x, height + transform.position.y + deltaY, transform.position.z);
 				transform.position = Vector3.SmoothDamp (transform.position, destination, ref velocity, damptime, Mathf.Infinity, Time.deltaTime);
-				gameObject.GetComponent<Camera> ().orthographicSize = 10; //test
+				gameObject.GetComponent<Camera> ().orthographicSize = 8; //test
 			}
 		}
 	}
